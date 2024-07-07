ea295562ea3a03fcd2b2e6997ba0c25d0d548073
virshu <felix@rabinovich.org>
2024-07-06 23:19:02 -0700

    Added Aurora Serverless (Lab 5)
    https://catalog.us-east-1.prod.workshops.aws/workshops/5c59492e-fc61-4a7b-b08a-e0307b70f390/en-US/5-postgre-sql

--- src/NorthwindCdk/NorthwindCdkStack.cs
+++ src/NorthwindCdk/NorthwindCdkStack.cs
@@ -5,7 +5,6 @@ using Constructs;
 using System.Net.Http;
 using Amazon.CDK.AWS.RDS;
 using Amazon.CDK.AWS.SSM;
-// ReSharper disable ObjectCreationAsStatement
 
 namespace NorthwindCdk;
 
@@ -14,7 +13,10 @@ public class NorthwindCdkStack : Stack
     internal  NorthwindCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
     {
         // Create VPC
-        Vpc vpc = new(this, "LabVpc", new VpcProps());
+        Vpc vpc = new(this, "LabVpc", new VpcProps
+        {
+            IpAddresses = IpAddresses.Cidr("10.3.0.0/16")
+        });
 
         // Generate password and store it in Secrets Manager
         Secret dbPassword = new(this, "LabDbPassword", new SecretProps
@@ -37,7 +39,7 @@ public class NorthwindCdkStack : Stack
         });
 
 #pragma warning disable S1075
-        string externalIp =  new HttpClient().GetStringAsync("http://icanhazip.com").Result.Replace("\n", "/32");
+        string externalIp =  new HttpClient().GetStringAsync("http://ipinfo.io/ip").Result + "/32";
 #pragma warning restore S1075
         sg.AddIngressRule(Peer.Ipv4(externalIp), Port.Tcp(1433)); // SQL Server
 
@@ -83,6 +85,51 @@ public class NorthwindCdkStack : Stack
         new CfnOutput(this, "SQLServerConnectionString", new CfnOutputProps
         {
             Value = connectionString.StringValue
+        });
+
+        // PostgreSQL setup
+        sg.AddIngressRule(Peer.Ipv4(externalIp), Port.Tcp(5432)); // PostgreSQL
+
+        // Amazon Aurora Serverless v2
+        DatabaseCluster postgreSql = new(this, "AuroraCluster", new DatabaseClusterProps
+        {
+            Vpc = vpc,
+            // you need to access database from your developer PC
+            VpcSubnets = new SubnetSelection() { SubnetType = SubnetType.PUBLIC },
+            SecurityGroups = new ISecurityGroup[] { sg },
+
+            // Amazon Aurora Serverless v2
+            Engine = DatabaseClusterEngine.AuroraPostgres(new AuroraPostgresClusterEngineProps
+            {
+                Version = AuroraPostgresEngineVersion.VER_16_2
+            }),
+            Writer = ClusterInstance.ServerlessV2("northwind-writer", new ServerlessV2ClusterInstanceProps {
+                InstanceIdentifier = "northwind-writer"
+            }),
+            ServerlessV2MinCapacity = 0.5,
+            ServerlessV2MaxCapacity = 1,
+
+            Credentials = Credentials.FromPassword(
+                username: dbUser,
+                password: dbPassword.SecretValue),
+            ClusterIdentifier = "northwind-postgresql",
+            DefaultDatabaseName = "NorthwindTraders",         
+            RemovalPolicy = RemovalPolicy.DESTROY // you need to be able to delete database  
+        });
+
+        // Store Aurora PostgreSQL connection string in Systems Manager Parameter Store
+        StringParameter auroraConnectionString = new(this, "NorthwindPostgreSQLDatabaseConnectionString", new StringParameterProps
+        {
+            ParameterName = "/Northwind/ConnectionStrings/NorthwindAuroraDatabase",
+            Description = "PostgreSQL connection string",
+            StringValue =
+                $"Server={postgreSql.ClusterEndpoint.Hostname};Database=NorthwindTraders;Username={dbUser};Password={dbPassword.SecretValue.UnsafeUnwrap()}"
+        });
+
+        // Display connection string as output
+        new CfnOutput(this, "AuroraConnectionString", new CfnOutputProps
+        {
+            Value = auroraConnectionString.StringValue
         });
     }
 }
